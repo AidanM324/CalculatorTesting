6a3ff86e3e0c68b5e794b5ee297033e5c7ccac47
Aidan Malone <G00423306@atu.ie>
2024-04-12 11:25:51 +0100

    Started Week10 CalcTest

---
+++ src/main/java/ie/atu/Calculator.java
@@ -0,0 +1,9 @@
+package ie.atu;
+
+public class Calculator {
+    public int add(int firstNum, int secondNum)
+    {
+        int result = firstNum + secondNum;
+        return result;
+    }
+}
--- src/test/java/ie/atu/week10/CalculatorTest.java
+++ src/test/java/ie/atu/week10/CalculatorTest.java
@@ -1,4 +1,11 @@
 package ie.atu.week10;
 
 public class CalculatorTest {
+    Calculator newCalc;
+
+    void testAdd()
+    {
+        newCalc = new Calculator();
+        int result = newCalc.add(4,6);
+    }
 }
